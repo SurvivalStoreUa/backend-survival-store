10720b9a3ec11faef1c975efedecce9b06998857
Oleksandr Klymenko <117536388+kllymenko@users.noreply.github.com>
2024-09-16 11:14:10 +0300

    Add files via upload

---
+++ src/main/java/ua/teamchallenge/survivalstore/SurvivalstoreApplication.java
@@ -0,0 +1,13 @@
+package ua.teamchallenge.survivalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivalstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivalstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/teamchallenge/survivalstore/SurvivalstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.teamchallenge.survivalstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvivalstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
